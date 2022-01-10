39e3503bfc9a3b773667ad6ab20dd98e03c8b7b4
Amy Hurford <ahurford@mun.ca>
2022-01-10 17:23:26 -0330

    Add files via upload

---
+++ Example_Script.R
@@ -0,0 +1,12 @@
+# Title: Example script
+# Author: Amy Hurford
+# Date: Sept 24, 2020
+
+# x-values of the graph
+x<-c(1,2,4)
+
+# y-values of the graph
+y<-c(2,2,2)
+
+# Making the graph
+plot(x,y)
